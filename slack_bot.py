24be03bf25f034de7a033d19a5ff1b75bcf4b9ad
Dape224 <oladaposaa2006@gmail.com>
2026-09-04 19:34:41 +0100

    Slack error

--- slack_bot.py
+++ slack_bot.py
@@ -138,5 +138,5 @@ if __name__ == "__main__":
     threading.Thread(target=run_web, daemon=True).start()
 
     handler = SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN"))
-    print("⚡ Slack bot is running...")
+    print("Slack bot is running...")
     handler.start()
