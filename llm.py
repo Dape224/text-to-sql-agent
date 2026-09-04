3297d15d442c6616f4e578ae7567d3b926b9254d
Dape224 <oladaposaa2006@gmail.com>
2026-09-04 17:55:49 +0100

    LLM model change

--- llm.py
+++ llm.py
@@ -8,7 +8,7 @@ api_key= os.getenv("MISTRAL_API_KEY")
 
 def brain():
     llm = ChatMistralAI(
-        model= "mistral-medium-latest",
+        model= "mistral-medium-2505",
         api_key= api_key
     )
     return llm
