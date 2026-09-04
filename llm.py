be17c5c2bc3a0aa9aa5732cf244334c970123cc5
Dape224 <oladaposaa2006@gmail.com>
2026-09-04 19:14:44 +0100

    Error fiexed

--- llm.py
+++ llm.py
@@ -8,7 +8,7 @@ api_key= os.getenv("GEMINI_API_KEY")
 
 def brain():
     llm = ChatGoogleGenerativeAI(
-        model= "gemini-2.5-flash"",
+        model= "gemini-2.5-flash",
         api_key= api_key
     )
     return llm
